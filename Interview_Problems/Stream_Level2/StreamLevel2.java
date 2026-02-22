28638f40f7108a14c04038458f7389a8674988f1
Hirthuk <sharankumarp02@gmail.com>
2026-02-22 15:02:51 +0530

    Stream Employee half

--- Interview_Problems/Stream_Level2/StreamLevel2.java
+++ Interview_Problems/Stream_Level2/StreamLevel2.java
@@ -1,8 +1,9 @@
 package Interview_Problems.Stream_Level2;
 
-import java.util.Comparator;
+import java.awt.*;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 public class StreamLevel2 {
@@ -34,12 +35,83 @@ public class StreamLevel2 {
         return employees.stream().max(Comparator.comparingDouble(Employee::getSalary)).orElse(null);
     }
 
-//    Find the highest paid employee in each department. Return Return Map<Department, Employee>
+//    Find the highest paid employee in each department.  Return Map<Department, Employee>
+    public static Map<String, Employee> highPaidDepartment(List<Employee> list) {
+        Map<String, Employee> result = list.stream().collect(
+                Collectors.groupingBy(Employee::getDepartment,
+                        Collectors.collectingAndThen(
+                                Collectors.maxBy(Comparator.comparingDouble(Employee:: getSalary)) ,
+                                Optional:: get
+                        )
+                        )
+        );
+        return result;
+    }
+
+//    Find average salary of each department.
+    public static Map<String, Double> averageSalaryDepartment(List<Employee> list) {
+        Map<String, Double> result = list.stream().collect(
+                Collectors.groupingBy(Employee:: getDepartment,
+                        Collectors.averagingDouble(Employee:: getSalary)
+                        )
+        );
+        return result;
+
+    }
+
+//    Group employees by gender.
+    public static Map<String, List<Employee>> groupByGender(List<Employee> list) {
+        Map<String, List<Employee>> result = list.stream().collect(Collectors.groupingBy
+                (Employee::getGender));
+        return result;
+    }
+
+//Find the department with the highest average salary
+    public static String highestAverageDepartment(List<Employee> list) {
+        Map<String, Double> averageSalaryPerDepartment = list.stream().collect(
+                Collectors.groupingBy(Employee::getDepartment,
+                Collectors.averagingDouble(Employee::getSalary)
+        ));
+                Optional<Double> result = averageSalaryPerDepartment.values().stream().max(Comparator.naturalOrder());
+
+                String salaryDepartment = averageSalaryPerDepartment.keySet().stream().filter(e ->
+                        averageSalaryPerDepartment.get(e) == result.get()).findFirst().orElse("NoOne");
+                return salaryDepartment;
+
+    }
+
+//    Find the youngest employee.
+    public static Employee youngEmployee(List<Employee> list) {
+        return list.stream().min(Comparator.comparingInt(Employee:: getAge)).get();
+    }
+
+//    Sort employees by salary descending, then by age ascending.
+    public static List<Employee> sortBySalaryAge(List<Employee> list) {
+        return list.stream().sorted(Comparator.comparingDouble(Employee:: getSalary).reversed())
+                .sorted(Comparator.comparingInt(Employee:: getAge)).collect(Collectors.toList());
+    }
+
+//    Get the top 3 highest paid employees.
+    public static List<Employee> top3PaidEmployee(List<Employee> list) {
+        return list.stream().sorted(Comparator.comparing(Employee:: getSalary).reversed()).limit(3).collect(
+                Collectors.toList()
+        );
+    }
+
+//    Partition employees into salary > 80k and <= 80k.
+    public static Map<String, List<Employee>> employeebased80k(List<Employee> list) {
+//        return list.stream().collect(Collectors.partitioningBy(employee -> employee.getSalary() > 80000));
+        return list.stream().collect(Collectors.groupingBy
+                (employee -> employee.getSalary() > 80000 ? "High Package" : "Low Package")
+                );
+    }
+
+
     public static void main(String[] args) {
 
         List<Employee> employees = List.of(
                 new Employee(1, "Sharan", "IT", 80000, 26, "Male"),
-                new Employee(2, "Priya", "HR", 60000, 30, "Female"),
+                new Employee(2, "Priya", "HR", 60000, 24, "Female"),
                 new Employee(3, "Arjun", "IT", 95000, 28, "Male"),
                 new Employee(4, "Sneha", "Finance", 75000, 32, "Female"),
                 new Employee(5, "Rahul", "Finance", 72000, 29, "Male"),
@@ -52,6 +124,15 @@ public class StreamLevel2 {
         System.out.println(salaryBased(employees));
         System.out.println(countPerDepartment(employees));
         System.out.println(highestPaid(employees));
+        System.out.println(highPaidDepartment(employees));
+        System.out.println(averageSalaryDepartment(employees));
+        System.out.println(groupByGender(employees));
+        System.out.println(highestAverageDepartment(employees));
+        System.out.println(youngEmployee(employees));
+        System.out.println(sortBySalaryAge(employees));
+        System.out.println(top3PaidEmployee(employees));
+        System.out.println(employeebased80k(employees));
+
     }
 
 }
